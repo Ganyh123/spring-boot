922db1a9edc924caa95ffbea554043ea8efdea8d
boyzgan <2357842073@qq.com>
2021-03-25 21:12:20 +0800

    多表查询

---
+++ spring-boot-config/src/test/java/com/gyhstudy/springboot/config/SpringBootConfigApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gyhstudy.springboot.config;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootConfigApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
